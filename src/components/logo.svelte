d09ab509bb47c088f31b53f80d84a2337cef9fb2
Jay Jung <mzc02-jayj@MZC02-JAYJ.local>
2019-08-22 22:48:57 +0900

    헤더에 로고 삽입!

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -9,9 +9,27 @@
 </script>
 
 <style>
-  svg {
-    width: 100%;
-    height: 100%;
+  .logo-container {
+    width: 100px;
+    margin: 0px 10px;
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo-symbol {
+    flex: 0 0 20px;
+  }
+  .logo-type {
+    flex: 0 0 60px;
+    max-width: 60px;
+  }
+  .logo-korea {
+    font-size: 12px;
+    flex: 0 0 10px;
+    align-self: flex-end;
+    text-align: left;
+    position: relative;
+    top: -1px;
   }
 
   path {
@@ -20,20 +38,25 @@
   }
 </style>
 
-<h1>test</h1>
-
-{#if visible}
+<section class="logo-container">
   <!-- content here -->
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
-    <g out:fade={{ duration: 200 }} opacity="0.2">
-      <path
-        in:expand={{ duration: 400, delay: 1000, easing: quintOut }}
-        style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
-        d={outer} />
-      <path
-        in:draw={{ duration: 1000 }}
-        style="stroke:#ff3e00; stroke-width: 1.5"
-        d={inner} />
-    </g>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 103 124"
+    class="logo-symbol">
+    {#if visible}
+      <g out:fade={{ duration: 200 }} opacity="1">
+        <path
+          in:expand={{ duration: 400, delay: 1000, easing: quintOut }}
+          style="stroke: #ff3e00; fill: #ff3e00; stroke-width: 50;"
+          d={outer} />
+        <path
+          in:draw={{ duration: 1000 }}
+          style="stroke:#ff3e00; stroke-width: 1.5"
+          d={inner} />
+      </g>
+    {/if}
   </svg>
-{/if}
+  <img alt="Svelte logotype" class="logo-type" src="svelte-logotype.svg" />
+  <span class="logo-korea">.kr</span>
+</section>
